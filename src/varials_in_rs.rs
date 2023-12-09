d34a26a46c49f0275c4f75755207e17920230fe0
Mohammed Yussif <mohammed.yussif@invennico.com>
2023-12-09 02:43:44 +0000

    tuples and arrays

--- src/varials_in_rs.rs
+++ src/varials_in_rs.rs
@@ -12,6 +12,15 @@ pub fn variables_in_rust() {
     //nMutable and Const variables: {VCONST}
     const VCONST: u32 = 60* 60* 4;
 
+
+    //tuples 
+    let tup: (i32, i64, f64, u8) = (2,100, 30.67, 1);
+
+
+    // Array
+    let arr_type_one = [0,1,2,3,4,5,6];
+    let arr_type_two:[i64; 5] = [30,10,20,60,40];
+
     let x = 5;
 
     let x = x+1;
@@ -25,6 +34,26 @@ pub fn variables_in_rust() {
     println!("Mutable variables: {v2}");
     println!("UnMutable variables: {v3}");
     println!("UnMutable and Const variables: {VCONST}");
+
+    // using spread on tuples
+    let (a,b,c,d,) = tup;
+
+    // getting single tuple values
+    let y = tup.0;
+    let z = tup.3;
+    println!("tuples {a}");
+    println!("tuples {b}");
+    println!("tuples {c}");
+    println!("tuples {d}");
+    println!("======= Single tuple values ============");
+    println!("tuples {y}");
+    println!("tuples {z}");
+
+    println!("=========== Array values ============");
+    let fist = arr_type_one[0];
+    let second = arr_type_two[0];
+    println!("Array One: {fist} ");
+    println!("Array One: {second}");
 
    
 }
