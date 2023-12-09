029dd5aa2480de9e67bc22ee40d518a2641ac974
Mohammed Yussif <mohammed.yussif@invennico.com>
2023-12-09 01:07:00 +0000

    vars

--- src/varials_in_rs.rs
+++ src/varials_in_rs.rs
@@ -12,6 +12,16 @@ pub fn variables_in_rust() {
     //nMutable and Const variables: {VCONST}
     const VCONST: u32 = 60* 60* 4;
 
+    let x = 5;
+
+    let x = x+1;
+    {
+        let x= x *2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+      println!("The value of x is: {x}");
+
     println!("Mutable variables: {v2}");
     println!("UnMutable variables: {v3}");
     println!("UnMutable and Const variables: {VCONST}");
