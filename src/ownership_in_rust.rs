ca6337b317a649b748817fb2b4bdc6ad9cec4d4c
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-01-07 00:30:56 +0000

    ownership with string, functions and int

--- src/ownership_in_rust.rs
+++ src/ownership_in_rust.rs
@@ -28,4 +28,38 @@ pub fn ownership_rs_tuto() {
     println!("Y value: {}", y);
 
     // Ownership and Functions
+
+    let s4 = String::from("Hello"); // s4 comes into scope
+
+    takes_ownership(s4); // s4's value moves into the function and so is no longer valid here
+
+    let x1 = 5; // x1 comes into scope
+
+    makes_copy(x1); // x1 would move into the function,
+                    // but i32 is copy, so it's okay to still use x afterward
+
+    // Return Values and Scope
+    let s5 = gives_ownership(); // gives_ownership moves its return value into s5
+
+    let s6 = String::from("hello"); // s6 comes into scope
+
+    let s7 = takes_and_gives_back(s6); // s6 is moved into
+                                       // takes_and_gives_back, which also moves its return value into s7
+}
+
+fn takes_ownership(some_string_string: String) {
+    println!("Takes ownership value: {}", some_string_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("Makes a copy value: {}", some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
