f11eb43a0d889b8ce3b13e4451241fb7951691b6
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-01-07 02:19:35 +0000

    references 1

--- src/references_borrowing.rs
+++ src/references_borrowing.rs
@@ -1,7 +1,9 @@
 pub fn references_main_func() {
     let s = String::from("Coded");
 
-    let len = calculate_length(&s);
+    let len = calculate_length(&s); // &s reference of s instead moving it
+                                    // The ampersands represent references, and it allow you to
+                                    // refer to some value without taking ownership of it
     println!("The length of {} String is, {}", s, len);
 }
 
