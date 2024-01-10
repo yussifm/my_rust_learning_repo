2ede3498264acec1ed1e939494308c22f023c5ce
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-01-10 09:16:27 +0000

    .

--- src/structures_in_rust.rs
+++ src/structures_in_rust.rs
@@ -5,16 +5,15 @@ struct User {
     email: String,
     sign_in_counts: u64,
 }
-
+struct  AlwaysEqual;
 
 // Tuple struct
-struct Color(i32, i32,i32); 
-struct Points(i32, i32,i32); 
+struct Color(i32, i32, i32);
+struct Points(i32, i32, i32);
 
 pub fn structures_in_rust() {
-
-    let color_1 = Color(0,1,2);
-    let point_1= Points(0,0,0);
+    let color_1 = Color(0, 1, 2);
+    let point_1 = Points(0, 0, 0);
 
     let user_1: User = User {
         active: true,
@@ -23,7 +22,16 @@ pub fn structures_in_rust() {
         sign_in_counts: 1,
     };
 
-        println!("======== Normal Struct ==========",);
+    // Unit-Like Structs Without Any Fields
+    //A structs that doesn't have any fields.
+    //Unit-Like structs can be useful when you
+    //need to implement a trait on some type but
+    // don't have any data that you want to store in the type itself.
+    let subject = AlwaysEqual;
+
+
+
+    println!("======== Normal Struct ==========",);
     println!("User Name: {}", user_1.username);
     println!("Is User active: {}", user_1.active);
     println!("User Email: {}", user_1.email);
